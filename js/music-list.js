eae43b03a3aaa04dc23022fa1297863a2b39c56c
Gajendra Prajapati <111476603+Gajenda7@users.noreply.github.com>
2022-10-07 14:20:59 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+let allMusic = [
+
+{
+
+  name: "Shri Krishna Govind Hare",
+  artist: "Maanya Arora",
+  img: "music-1",
+  src: "music-1"
+},
+{
+  name: "Achutam Keshavam",
+  artist: "Ankit Batra",
+  img: "music-2",
+  src: "music-2"
+},
+{
+  name: "Mera Safar",
+  artist: "Eqlipse Nova",
+  img: "music-3",
+  src: "music-3"
+},
+{
+  name: "Chak len de",
+  artist: "Kailash Kher",
+  img: "music-4",
+  src: "music-4"
+},
+{
+  name: "Kho Gae Hum Kahan",
+  artist: "Jasleen Royal",
+  img: "music-5",
+  src: "music-5"
+},
+{
+  name: "Tauba",
+  artist: "Badshah",
+  img: "music-6",
+  src: "music-6"
+},
+];
